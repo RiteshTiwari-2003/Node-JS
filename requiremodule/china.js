5a3f5a300c6989a65cde46f52c9598774f8a2613
Ritesh <riteshtiwari3112003@gmail.com>
2025-04-30 15:57:30 +0530

    i have added notes on require import and export

--- requiremodule/china.js
+++ requiremodule/china.js
@@ -1,5 +1,24 @@
 const allImports=require("./india.js");
-console.log(allImports);
+//console.log(allImports);
+allImports.carfunc();
 
 // so one question is how to find main file for run in india.js and china.js then in main file we import the thing 
 // so i can say that china.js is main file so run the node china.js
+/**even though when  i want to buy china mobile then i can not directly import from india first before import only when china export it only that time we can import it 
+ * so export is the first thing that has to happen 
+ * 
+ * 2nd case : like one caseis about that china want to export many thing but india want to import only some thing like china want to export 
+ * potatao but india already made production of potato same like for toys so for this we use destructuring like 
+ * 
+ * in china.js file i only want to import phones from them then we can write like this 
+ * 
+ * const {phones}=require("./india.js")
+ * console.log(phones);
+ * 
+ * default 
+ * like what is default import like when we write like this 
+ * const vikram=require("./india.js")
+ * console.log(vikram); // then in this all export things store into vikram this is called as default import 
+ * 
+ * 
+ */
--- requiremodule/india.js
+++ requiremodule/india.js
@@ -2,11 +2,15 @@ const phones=["Xiomi","samsung","Nokia","Apple","Oneplus","Vivo","oppo"]
 const toys={"name":"supermantoy"}
 const potato=1000
 const equipment="manifacturing machinary"
+const car=function(){
+    console.log("made in china");
+}
 module.exports={
     "phones":phones,
     "toys":toys,
     "Organinc_potato":potato,
-    "equipment":equipment
+    "equipment":equipment,
+    "carfunc":car
 }
 
 /**i said there are two way to import and export thing so this is called one pattern called as common.js in common.js we want to 
