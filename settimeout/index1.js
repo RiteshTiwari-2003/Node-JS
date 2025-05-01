17d520d9a41a2cad546a088da7fcd544afe54585
Ritesh <riteshtiwari3112003@gmail.com>
2025-05-01 15:20:28 +0530

    i havr added some notes on callback function call in settimeout

--- settimeout/index1.js
+++ settimeout/index1.js
@@ -20,14 +20,41 @@ function task2(){
 task1();
 task2();//output of above is this that task2 is completed , task1 is completed 
 
-function step1(){
-    setTimeout(()=>{
-        return "task1 result";
+// function step1(){
+//     setTimeout(()=>{
+//         return "task1 result";
+//     },2000);
+// }
+// function step2(resultfromStep1){
+//     console.log(resultfromStep1+"task 2 completed");
+// }
+// const result1=step1();
+// const result2=step2(result1);
+// console.log(result2);//output is undefined task 2 completed  so solution of above is below
+
+// function step1(){
+//     setTimeout(()=>{
+//         step2("step1 result");
+//     },2000);
+// }
+// function step2(resultFromStep1){
+//     console.log(resultFromStep1+"task2 result");
+// }
+// step1();//output is step1 result task2 result after 2 second come 
+
+//now next method for function calling using settimeout also this is 
+function step1(callback){
+    settimeout(()=>{
+        callback("step1 result");
     },2000);
 }
-function step2(resultfromStep1){
-    console.log(resultfromStep1+"task 2 completed");
+function step2_alternative(resultfromStep1){
+    console.log(resultfromStep1+"-----"+"task2 result");
 }
-const result1=step1();
-const result2=step2(result1);
-console.log(result2);//output is undefined task 2 completed
+function step2(resultFromStep1){
+         console.log(resultFromStep1+"task2 result");
+     }
+step1(step2_alternative);
+step1(step2);
+
+
