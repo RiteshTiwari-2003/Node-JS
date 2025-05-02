77c037cf2b42e2d85ce40648cce1d28915d49413
Ritesh <riteshtiwari3112003@gmail.com>
2025-05-02 16:46:08 +0530

    i have understand difference between callback hell and promise

--- promise.js
+++ promise.js
@@ -22,3 +22,42 @@ vikram().then(abdul).then(ramesh);;
 
 //     }
 // }
+function boilWater(){
+    return new Promise((resolve)=>{
+        console.log("Step1: water is boiled");
+        resolve("boiled water");
+    });
+}
+function addpasta(previousStepResult){
+    return new Promise((resolve)=>{
+        console.log(`step2: pasta is added to ${previousStepResult}`);
+        resolve("cooked pasta");
+    });
+}
+function drainWater(previousStepResult){
+    return new Promise((resolve)=>{
+        console.log(`step3: drained water from ${previousStepResult}`);
+        resolve("drained pasta");
+    });
+}
+function addSauceAndServe(previousStepResult){
+    return new Promise((resolve)=>{
+        console.log(`step4: added sauce to ${previousStepResult} and serve to`);
+        resolve("delicious pasta");
+
+    });
+}
+//callback hell approach
+boilWater((boiledWater)=>{
+    addpasta(boiledWater,(cookedPasta)=>{
+        drainWater(cookedPasta,(drainedPasta)=>{
+            addSauceAndServe(drainedPasta,(finalDish)=>{
+                console.log(`final result is :${finalDish}`);
+            })
+        })
+    })
+})
+//chaining promise 
+boilWater().then(addpasta).then(drainWater).then(addSauceAndServe).then((finalDish)=>{
+    console.log(`final dish is ${finalDish}`);
+})
