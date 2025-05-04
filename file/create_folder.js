bf4b5cb943dda9edef0c689573476c3acc408caa
Ritesh <riteshtiwari3112003@gmail.com>
2025-05-05 00:42:40 +0530

    i have added some notea

--- file/create_folder.js
+++ file/create_folder.js
@@ -6,3 +6,17 @@ fs.mkdir('newDir',(err)=>{
     }
     console.log("directory created successfully");
 });
+/**how to remove directory 
+ * 
+ * const fs=require('fs');
+ * fs.rmdir('newDir',(err)=>{
+ * if(err){
+ * console.error(err);
+ * return;}
+ * console.log("directory deleted");})
+ * 
+ * if you want to delete the file you can also use unlink method 
+ * const fs=require('fs');
+ * fs.unlink('newDir',(err)=>{
+ * console.log("file deleted successfully");})
+ */
