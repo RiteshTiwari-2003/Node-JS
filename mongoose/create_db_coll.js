1dba9ea83d3312b4aefb7b2a0e9ed8ab12151163
Ritesh <riteshtiwari3112003@gmail.com>
2025-05-22 19:09:54 +0530

    i have created schema and model

--- mongoose/create_db_coll.js
+++ mongoose/create_db_coll.js
@@ -24,3 +24,10 @@ async function createSchemaAndModel(){
 
 }
 createSchemaAndModel();
+/**for every collection 
+ * step 1 : create schema 
+ * step 2; create model 
+ * i will try to create schema objet and after creating schema object we will pass the schema object to the model 
+ * and for every collection like insertion deletion updation  i will do thing on model student modelor any model do validation internally 
+ * 
+ */
